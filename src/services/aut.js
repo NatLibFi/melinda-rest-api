179ddb811f395a0c889cee79ef268326882955f0
Aki Seppänen <aki.seppanen@gofore.com>
2018-01-17 09:07:42 +0200

    Add copyright disclaimers to generated files

--- src/services/aut.js
+++ src/services/aut.js
@@ -1,6 +1,34 @@
 /* eslint-disable no-unused-vars, valid-jsdoc */
 
 /**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* RESTful API for Melinda
+*
+* Copyright (C) 2018 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-rest-api
+*
+* melinda-rest-api program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-rest-api is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/**
  * @param {Object} options
  * @param {Boolean} options.noop Do not create the record but return the messages about the operation
  * @param {Boolean} options.unique Do not create the record if there are duplicates in the datastore
