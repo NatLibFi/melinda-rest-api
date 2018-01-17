7ddfdecb8778dd66031eb596ce15cd850e28b30c
Aki Seppänen <aki.seppanen@gofore.com>
2018-01-17 10:54:47 +0200

    Create test stubs for services

---
+++ test/bib-service.spec.js
@@ -0,0 +1,60 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* RESTful API for Melinda
+*
+* Copyright (C) 2018 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-rest-api
+*
+* melinda-rest-api program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-rest-api is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+/* eslint-disable no-unused-vars, no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+
+import {expect} from 'chai';
+import * as testContext from '../src/services/bib';
+
+describe.skip('services/bib', () => {
+	it('postBibRecords', async () => {
+		const result = await testContext.postBibRecords();
+	});
+
+	it('postBibRecordsById', async () => {
+		const result = await testContext.postBibRecordsById();
+	});
+
+	it('getBibRecordsById', async () => {
+		const result = await testContext.getBibRecordsById();
+	});
+
+	it('postBibRecordsByIdLock', async () => {
+		const result = await testContext.postBibRecordsByIdLock();
+	});
+
+	it('deleteBibRecordsByIdLock', async () => {
+		const result = await testContext.deleteBibRecordsByIdLock();
+	});
+
+	it('getBibRecordsByIdLock', async () => {
+		const result = await testContext.getBibRecordsByIdLock();
+	});
+});
