e08e4ef73bed205e0b754151fa17bf9f0c46600e
Aki Seppänen <aki.seppanen@gofore.com>
2018-02-23 09:17:40 +0200

    refactor: move aleph auth to own file

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,14 +1,6 @@
 import passport from 'passport';
 import {BasicStrategy} from 'passport-http';
-import axios from 'axios';
-import {promisify} from 'es6-promisify';
-import xml2js from 'xml2js';
-import {readEnvironmentVariable} from '../utils';
-
-const parseXMLStringToJSON = promisify(xml2js.parseString);
-
-const alephUrl = readEnvironmentVariable('ALEPH_URL');
-const alephUserLibrary = readEnvironmentVariable('ALEPH_USER_LIBRARY');
+import alephAuth from './aleph';
 
 export default () => {
 	passport.serializeUser((user, done) => {
@@ -19,25 +11,9 @@ export default () => {
 		done(null, user);
 	});
 
-	passport.use(new BasicStrategy(async (username, password, done) => {
-		const requestUrl = `${alephUrl}/X?op=user-auth&library=${alephUserLibrary}&staff_user=${username}&staff_pass=${password}`;
-
-		axios(requestUrl)
-			.then(response => response.data)
-			.then(parseXMLStringToJSON)
-			.then(json => {
-				const credentialsValid = json['user-auth'].reply[0];
-
-				if (credentialsValid) {
-					const userLibrary = json['user-auth'].z66[0]['z66-user-library'][0];
-					const name = json['user-auth'].z66[0]['z66-name'][0];
-					const department = json['user-auth'].z66[0]['z66-department'][0];
-					const email = json['user-auth'].z66[0]['z66-email'][0];
-					done(null, {username, userLibrary, name, department, email});
-				} else {
-					done(null, false);
-				}
-			})
+	passport.use(new BasicStrategy((username, password, done) => {
+		alephAuth(username, password)
+			.then(response => done(null, response))
 			.catch(err => done(err));
 	}));
 };
---
+++ test/auth.spec.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars, no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+
+import {expect} from 'chai';
+import * as testContext from '../src/services/bib';
+
